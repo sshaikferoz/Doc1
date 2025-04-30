08cd9441bfad31e4463fe21629dfd7361c2e5628
Shaik Feroz Hussain <sshaikferoz@kaaretech.com>
2025-04-30 17:37:50 +0300

    commit1

--- components/common/AlertBox.js
+++ components/common/AlertBox.js
@@ -12,6 +12,7 @@ export default function Alerts({
   StrokeColor,
 }) {
   //prettier-ignore
+
   return (
     <div style={style} className={styles.alertItemSVG}>
       {/* <img className={styles.box} src={`${process.env.NEXT_PUBLIC_BSP_NAME}/images/alert-box-shadow.svg`} /> */}
@@ -58,5 +59,5 @@ export default function Alerts({
         </div>
       </div>
     </div>
-  )
+    )
 }
